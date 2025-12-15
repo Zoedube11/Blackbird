c759f7088005dd8411d3eae3c3c904c186cdc320
Zoedube11 <zoe@skxconsulting.co.za>
2025-12-15 09:04:59 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      // This proxy will forward everything cleanly and follow redirects
-      "/proxy-api": {
-        target: "https://bb-booking-sys-production.up.railway.app/",
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/proxy-api/, ""),
-        followRedirects: true,
-      },
-      "/login": {
-        target: "https://bb-booking-sys-production.up.railway.app/",
-        changeOrigin: true,
-        secure: true,
-      },
     },
   },
 });
